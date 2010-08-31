717b6831d0ca3a92bf5fa7a0674bc6041ae97547
Digimer <digital.mermaid@gmail.com>
2010-08-30 20:02:54 -0400

    Much cleaned up, with file loading from command line switch.

--- diximal.c
+++ diximal.c
@@ -13,124 +13,88 @@ See README for usage and expected behaviour.
 
 // Inluded libraries.
 #include <stdio.h>		// Standard I/O
-// #include <errno.h>		// Error stuff
+#include <stdlib.h>		// Library for things like 'sizeof()', 'itoa()', rand(), etc.
+#include <unistd.h>		// Unix standard stuff.
+#include <errno.h>		// Error stuff
+#include <string.h>		// Verbose errors, etc.
 // #include <ctype.h>		// Library for testing and character manipulation.
 // #include <stdint.h>		// Library for standard integer types (guarantees the size of an int).
-#include <stdlib.h>		// Library for things like 'sizeof()', 'itoa()', rand(), etc.
 // #include <limits.h>		// Provides some integer and mathmatical functions line INT_MAX and UNIT_MAX
-#include <string.h>
 
-// File to read. Make this able to be picked up by a command line switch later.
-char xml_file[1024]="./source.xml";
+
+/* prototypes */
+void usage (char *name);
+
 
 // Main function.
-int main(void)
+int main(int argc, char *argv[])
 {
-	/* Variables */
-	// Maximum line length/buffer size.
-	char line[8096];
-	// Position of the newline in a line of the file. AKA, the length of a
-	// line.
-	int nl;
-	// Standard iterator.
-	int i;
-	// This gets set to 1 while inside a comment.
-	int in_comment;
-	// This gets set to 1 while inside a processing instruction.
-	int in_process_inst;
-	// This is set to 1 when I see the first non-space character is a string.
-	int seen_first_char;
-	// This is a counter to see how many characters I actually printed in a
-	// line.
-	int char_printed;
+	// Set variables.
+	char c;
+	char *read_file=NULL;
+	FILE *xml;
 	
-	// Open the XML file read-only.
-	FILE*xml;
-	xml=fopen(xml_file, "r");
-	
-	/* Initial values */
-	in_comment=0;
-	in_process_inst=0;
-	seen_first_char=0;
-	char_printed=0;
-	
-	// Exit with error code 1 if I couldn't read the file.
-	if (0 == xml)
+	// Pick up switches
+	while ((c = getopt (argc, argv, "hf:")) != -1)
 	{
-		printf("Failed to open %s\n", xml_file);
-		return 1;
-	}
-	
-	while (fgets(line, sizeof(line), xml))
-	{
-		// Reset the first char flag.
-		// MADI: Don't use this if I am in the middle of reading a
-		// MADI: multi-line CONTENT.
-		seen_first_char=0;
-		
-		// Reset the characters printed counter.
-		char_printed=0;
-		
-		// MADI: Make this work properly if a line exceeds the buffer
-		// MADI: (that is, 's' is NULL, concat the next line, etc).
- 		nl = strcspn (line, "\n");
-		
-		// Nix newlines.
-		line[nl]='\0';
-		
-		// Only proceed if the line isn't empty.
-		if (nl > 0)
+		switch (c)
 		{
-			for (i=0; i<nl; i++)
-			{
-				// Get rid of comments.
-				if (('<' == line[i]) && ('!' == line[i+1]) && ('-' == line[i+2]) && ('-' == line[i+3]))
-				{
-					in_comment=1;
-				}
-				else if (('-' == line[i]) && ('-' == line[i+1]) && ('>' == line[i+2]))
-				{
-					in_comment=0;
-					i+=3;
-				}
-				
-				// Get rid of processing instructions.
-				if (('<' == line[i]) && ('?' == line[i+1]))
-				{
-					in_process_inst=1;
-				}
-				else if (('?' == line[i]) && ('>' == line[i+1]))
-				{
-					in_process_inst=0;
-					i+=2;
-				}
-				
-				if ((0 == in_comment) && (0 == in_process_inst))
-				{
-					// Skip leading white spaces.
-					if ((0 == seen_first_char) && (' ' != line[i]) && '\t' != line[i])
-					{
-						seen_first_char=1;
-					}
-					if ((1 == seen_first_char) && (0 != line[i]))
-					{
-						printf("%c", line[i]);
-						char_printed++;
-					}
-				}
-			}
+			case 'h':
+				usage(argv[0]);
+				return 0;
+			break;
 			
-			// Don't print a newline if nothing was actually
-			// printed to the screen.
-			if (char_printed>0)
-			{
-				printf("\n");
-			}
-// 			printf("%s", line);
-// 			printf("line: %d-[%s]\n", s, line);
+			case 'f':
+				read_file = optarg;
+			break;
+			
+			case '?':
+				if (optopt == 'f')
+				{
+					printf("Missing filename.\n");
+				}
+				else
+				{
+					printf("Invalid switch '%c'\n", optopt);
+				}
+				usage(argv[0]);
+				return -1;
+			break;
+			
+			default:
+				printf("Unexpected error!\n");
+				usage(argv[0]);
+				return -2;
 		}
 	}
 	
+	// Did I get a file?
+	if (read_file == NULL)
+	{
+		printf("No file passed.\n");
+		usage(argv[0]);
+		return -3;
+	}
+	
+	// Read the file.
+	if ((xml = fopen(read_file, "r")) == NULL)
+	{
+		printf("Failed to read: [%s], error was: %s.\n", read_file, strerror(errno));
+		return -4;
+	}
+	
+	
+	
+	
+	
+	// Close the XML file.
+	fclose(read_file);
+	
 	// If I hit here, I am exiting normally.
 	return 0;
+}
+
+void usage (char *name)
+{
+	printf("Usage: %s -f /path/to/file.xml\n", name);
 }
