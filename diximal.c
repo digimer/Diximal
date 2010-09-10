86e564c4b4e8a2c109e7818f26be8c8445148bd9
Madison Kelly <mkelly@alteeve.com>
2010-09-09 23:33:27 -0400

    Now compiles with Fabio's updated Makefile and parses CDATA properly.
    This actually starts parsing the file_contents. It marks the start of '<' syntax with a \0 and recognizes the end of CDATA blocks.
    
    Signed-off-by: Madison Kelly <mkelly@alteeve.com>

--- diximal.c
+++ diximal.c
@@ -28,16 +28,14 @@ See README for usage and expected behaviour.
 
 #define STACK_SIZE	100
 
-/* prototypes */
-void usage (char *name);
-int strip_comments(char *contents, signed long size);
-
 
 /* Enumeration */
 
 typedef enum {
-	// These are being assigned sequential integer values by the compile (xml_content = 0, ... )
+	// These are being assigned sequential integer values by the compile
+	// (xml_content = 0, ... )
 	xml_none,	// Initial type.
+	xml_root,	// 
 	xml_content,	// While reading CONTENT.
 	xml_cdata,	// While in a CDATA block
 	xml_tag		// While parsing a tag.
@@ -46,8 +44,9 @@ typedef enum {
 
 /* Unions */
 
-// This has to come first as it's a forward decleration. This means that I can reference it in my structures before the unions define the
-// connections of the structures.
+// This has to come first as it's a forward decleration. This means that I can
+// reference it in my structures before the unions define the connections of
+// the structures.
 union xml_obj;
 
 /* structures */
@@ -56,49 +55,75 @@ typedef struct xml_attr_s {
 	char * name;
 	char * value;
 	struct xml_attr_s * next;
-} xml_attr_t;			// This name is use as a short-form for "struct xml_attr_s <name>".
+} xml_attr_t;			// This name is use as a short-form for "struct
+				// xml_attr_s <name>".
 
 // The name suffix _s helps show that it's a structure (vs. _t for type).
 // This is the generic function used to link things together.
 struct xml_generic_obj_s {
-	xml_obj_type_t obj_type;	// This is a variable of the named type (see in enumeration above)
-	union xml_obj * right;	// This points to ourself and points to whatever is to the right, or NULL.
+	// This is a variable of the named type (see in enumeration above)
+	xml_obj_type_t obj_type;
+	// This points to ourself and points to whatever is to the right, or
+	// NULL.
+	union xml_obj * right;
 };
 
 struct xml_content_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
-	char * xmldata;		// Contents between the opening tag and the first non-content element.
+	// Contents between the opening tag and the first non-content element.
+	char * xmldata;
 };
 
 struct xml_cdata_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
-	char * xmldata;		// This is the string between the opening and closing CDATA stanza.
+	// This is the string between the opening and closing CDATA stanza.
+	char * xmldata;
 };
 
 struct xml_tag_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
-	char * tagname;		// This is the name of the tag.
-	xml_attr_t * attributes;// This is a pointer to a linked list.
-	union xml_obj * children;// Pointer to chilren.
+	// This is the name of the tag.
+	char * tagname;
+	// This is a pointer to a linked list.
+	xml_attr_t * attributes;
+	// Pointer to chilren.
+	union xml_obj * children;
 };
+
+struct xml_root_obj_s {
+	// This, being root, has no siblings
+	xml_obj_type_t obj_type;
+	union xml_obj * children;
+};
+
 
 // Define the union.
 typedef union xml_obj {
 	xml_obj_type_t obj_type;
-	// If I didn't want to use 'struct ...', I could have put 'xml_generic_obj_t' after the decleration above.
+	// If I didn't want to use 'struct ...', I could have put
+	// 'xml_generic_obj_t' after the decleration above.
 	struct xml_generic_obj_s generic;
 	struct xml_content_obj_s content;
 	struct xml_cdata_obj_s   cdata;
+	struct xml_root_obj_s    xmlroot;
 	struct xml_tag_obj_s     xmltag;
 } xml_obj_t;
 
-// If I wanted to have garbage collection, I'd set any variables that I'd need to free as globals, set them to NULL, then on error function,
-// loop through and any that aren't NULL, free/close. I'd need to add each variable to the garbage collection function as I add them here so
-// that I know whether to 'free' or 'close'.
 
+/* prototypes */
+void usage (char *name);
+int strip_comments(char *contents, signed long size);
+xml_obj_t * parse_xml_content(char * file_contents);
+
+
+// If I wanted to have garbage collection, I'd set any variables that I'd need
+// to free as globals, set them to NULL, then on error function, loop through
+// and any that aren't NULL, free/close. I'd need to add each variable to the
+// garbage collection function as I add them here so that I know whether to
+// 'free' or 'close'.
 
 // Main function.
 int main(int argc, char *argv[])
@@ -107,28 +132,35 @@ int main(int argc, char *argv[])
 	char c;
 	char *read_file=NULL;
 	FILE *xml;
-	off_t buf_size;      // 'off_t' is defined in sys/types.h as the file size type. This usually equiv. to 'unsigned long' on *nix.
+	// 'off_t' is defined in sys/types.h as the file size type. This
+	// usually equiv. to 'unsigned long' on *nix.
+	off_t buf_size;
 	// MADI: Read up structures.
 	struct stat file_info;
 	// Initial file read buffer
 	char *file_contents;
 	// The root of the parsed stack
-	xml_obj_t * xml_root;
+	xml_obj_t * root;
 	
 	// Pick up switches
 	while ((c = getopt (argc, argv, "hf:")) != -1)
 	{
 		switch (c)
 		{
+			// Show usage
 			case 'h':
 				usage(argv[0]);
 				return 0;
 			break;
 			
+			// The file to read
 			case 'f':
 				read_file = optarg;
 			break;
 			
+			// '?' is set if an unknown argument is passed. If the previous
+			// option had a ':' after it without something after it, optopt
+			// will have the argument so you can report what was missing.
 			case '?':
 				if (optopt == 'f')
 				{
@@ -142,6 +174,8 @@ int main(int argc, char *argv[])
 				return -1;
 			break;
 			
+			// This is a catch all, but shouldn't be hit as '?'
+			// should catch all unknown switches.
 			default:
 				printf("Unexpected error!\n");
 				usage(argv[0]);
@@ -149,7 +183,8 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	// Create the stack. The '100' means that I can go as far as 100 child tags. This *should* be enough.
+	// Create the stack. The '100' means that I can go as far as 100 child
+	// tags. This *should* be enough.
 	if (create_stack(STACK_SIZE) != 0)
 	{
 		// Failed to allocate enough memory.
@@ -162,7 +197,7 @@ int main(int argc, char *argv[])
 	{
 		printf("No file passed.\n");
 		usage(argv[0]);
-		// MADI: There should be a 'free' before return to free
+		// MADI: There should be a 'free' before return.
 		return -3;
 	}
 	
@@ -174,7 +209,8 @@ int main(int argc, char *argv[])
 	}
 	
 	// Get the file info.
-	// MADI: pass a reference, which read_file is, and the address of file_info
+	// MADI: pass a reference, which read_file is, and the address of
+	// 'file_info'.
 	if (stat(read_file, &file_info) != 0)
 	{
 		printf("Failed to get the stat info on: [%s], error was: %s.\n", read_file, strerror(errno));
@@ -193,7 +229,8 @@ int main(int argc, char *argv[])
 	}
 	
 	// Read in the file to the buffer.
-	// This is intentionally backwards so that I read 'buf_size' once, rather that 1 byte 'buf_size' time.
+	// This is intentionally backwards so that I read 'buf_size' once,
+	// rather that 1 byte 'buf_size' time.
 	if ((fread(file_contents, file_info.st_size, 1, xml)) != 1)
 	{
 		printf("Failed to read the file: [%s], the error was: %s.\n", read_file, strerror(errno));
@@ -213,7 +250,7 @@ int main(int argc, char *argv[])
 	/* Start Parsing! */
 	
 	// Get the pointer to the root object. Parsing error if NULL.
-	if ((xml_root = parse_xml_content(file_contents)) != NULL)
+	if ((root = parse_xml_content(file_contents)) != NULL)
 	{
 		// I could use errno.
 		printf("Parsing error!");
@@ -252,16 +289,17 @@ int strip_comments(char *contents, signed long size)
 	}
 	if (contents[size-1] != 0)
 	{
-		// Contents doesn't end with a 0, contents are not to be trusted.
+		// Contents don't end with a 0, contents are not to be trusted.
 		return -2;
 	}
 	
-	// Because I am moving good data to the left and the sticking a NULL terminator at the end of the shrunken cleaned data, I 
-	// can use the same memory space.
+	// Because I am moving good data to the left and the sticking a NULL
+	// terminator at the end of the shrunken cleaned data, I can use the
+	// same memory space.
 	src=dst=contents;
 	
-	
-	// While I am getting data from source, and while I've not gone past my buffer length...
+	// While I am getting data from source, and while I've not gone past
+	// my buffer length...
 	while((*src != 0) && (size > 0))
 	{
 		// If this is the less-than sign, evaluate.
@@ -350,16 +388,21 @@ xml_obj_t * parse_xml_content(char * file_contents)
 	// Root object, initialized to NULL to make sure I'm not looking at garbage.
 	xml_obj_t * root_object = NULL;
 	xml_obj_type_t state_type = xml_none; // This stores the type of object we're in.
-	char * start = file_contents; // This will be used to keep track of start of data to store.
-	char * end   = file_contents; // This will be used to keep track of the end of data to store.
 	
 	// Current object
 	xml_obj_t * current_object;
-/*	if ((current_object = calloc(1, sizeof(xml_obj_t))) == NULL)
+	
+	// Allocate and wipe the memory for root_object.
+	if ((root_object = calloc(1, sizeof(xml_obj_t))) == NULL)
 	{
 		// Failed to allocate the memory.
 		return NULL;
-	}*/
+	}
+	
+	// Set the type of the root object.
+	root_object->obj_type = xml_root;
+	// the above is equiv. to "*root_object.type"
+	current_object = root_object;
 	
 	// Loop through until I hit 0.
 	while (*file_contents != 0)
@@ -368,8 +411,43 @@ xml_obj_t * parse_xml_content(char * file_contents)
 		// Do we have the start of some tag?
 		if (*file_contents == '<')
 		{
-			// Yar
-			if ((isalpha(*(file_contents+1)) != 0) || (*(file_contents+1) == '_'))
+			// Yar. < is ALWAYS a delimeter, so \0 it.
+			*file_contents = 0;
+			file_contents++;
+			// This is equal; *file_contents++ = 0; because it sets and /then/ increments.
+			
+			// Am I going in to a CDATA?
+			if (strncmp(file_contents, "![CDATA[", 8) == 0)
+			{
+				// Set the < to \0 so that I terminate the previous block.
+				file_contents += 8;	// Jump past '<![CDATA['
+				
+				// I want my sibling to point to this new XML object.
+				current_object->generic.right = calloc(1, sizeof(xml_obj_t));
+				if (current_object->generic.right == NULL)
+				{
+					// Failed to allocate memory.
+					return NULL;
+				}
+				
+				// Make the 'current_object' now point to the memory we just calloc'ed.
+				current_object = current_object->generic.right;
+				current_object->cdata.obj_type = xml_cdata;	// This lets me know later what I am looking at (xml_cdata)
+				current_object->cdata.xmldata = file_contents;	// This will mark the start of the CDATA.
+				
+				while ((*file_contents != ']') && (strncmp(file_contents+1, "]>", 2) != 0))
+				{
+					file_contents++;
+				}
+				
+				*file_contents = 0;	// This changes ']]>' to '\0]>'.
+				file_contents += 2;	// Jump past the ']>'
+				state_type = xml_none;	// Reset to xml_none as I don't know what I will be in the next loop around.
+			}
+			
+			
+			
+/*			else if ((isalpha(*(file_contents+1)) != 0) || (*(file_contents+1) == '_'))
 			{
 				// Tag name.
 				state_type = xml_tag;
@@ -388,9 +466,9 @@ xml_obj_t * parse_xml_content(char * file_contents)
 				// Bad formatting, can't parse.
 				// I could set 'errno = X' to make the caller print a useful error.
 				return NULL;
-			}
+			}*/
 		}
-		else if (strncmp(file_contents, "/>", 2) == 1)
+// 		else if (strncmp(file_contents, "/>", 2) == 1)
 			
 /*		}
 		// strncmp returns 0 on match, non-zero otherwise.
