e877bf7364b59518811e9016e8c1573782e5e641
Madison Kelly <mkelly@alteeve.com>
2010-09-11 14:59:04 -0400

    Cleaned up some comments and started work on processing tags.
    Signed-off-by: Madison Kelly <mkelly@alteeve.com>

--- diximal.c
+++ diximal.c
@@ -6,26 +6,40 @@ program.
 
 Madison Kelly (Digimer)
 mkelly@alteeve.com
-Aug. 28, 2010
+Sep. 11, 2010
 
 See README for usage and expected behaviour.
 */
 
-// Inluded libraries.
-#include <stdio.h>		// Standard I/O
-#include <stdlib.h>		// Library for things like 'sizeof()', 'itoa()', rand(), etc.
-#include <unistd.h>		// Unix standard stuff.
-#include <errno.h>		// Error stuff
-#include <string.h>		// Verbose errors, etc.
-#include <sys/types.h>		// Data types, structures, etc.
-#include <sys/stat.h>		// Get file details - See 'man 2 stat'
-#include <ctype.h>		// Library for testing and character manipulation.
-// #include <stdint.h>		// Library for standard integer types (guarantees the size of an int).
-// #include <limits.h>		// Provides some integer and mathmatical functions line INT_MAX and UNIT_MAX
+
+/* Inluded libraries. */
+
+// Standard I/O
+#include <stdio.h>
+// Library for things like 'sizeof()', 'itoa()', rand(), etc.
+#include <stdlib.h>
+// Unix standard stuff.
+#include <unistd.h>
+// Error stuff
+#include <errno.h>
+// Verbose errors, etc.
+#include <string.h>
+// Data types, structures, etc.
+#include <sys/types.h>
+// Get file details - See 'man 2 stat'
+#include <sys/stat.h>
+// Library for testing and character manipulation.
+#include <ctype.h>
+// I may not need these.
+// Library for standard integer types (guarantees the size of an int).
+// #include <stdint.h>
+// Provides some integer and mathmatical functions line INT_MAX and UNIT_MAX
+// #include <limits.h>
 
 // Include my header
 #include "stack.h"
 
+// This sets the limit of child tags I can delve in to.
 #define STACK_SIZE	100
 
 
@@ -49,14 +63,16 @@ typedef enum {
 // the structures.
 union xml_obj;
 
+
 /* structures */
 
+// This will be used for attributes and their values found in tags.
 typedef struct xml_attr_s {
 	char * name;
 	char * value;
 	struct xml_attr_s * next;
-} xml_attr_t;			// This name is use as a short-form for "struct
-				// xml_attr_s <name>".
+} xml_attr_t;	// This name is use as a short-form for "struct xml_attr_s
+		// <name>".
 
 // The name suffix _s helps show that it's a structure (vs. _t for type).
 // This is the generic function used to link things together.
@@ -68,6 +84,7 @@ struct xml_generic_obj_s {
 	union xml_obj * right;
 };
 
+// This is used for storing pointers to CONTENTS.
 struct xml_content_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
@@ -75,6 +92,7 @@ struct xml_content_obj_s {
 	char * xmldata;
 };
 
+// This is used for storing the pointers to CDATA contents.
 struct xml_cdata_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
@@ -82,6 +100,7 @@ struct xml_cdata_obj_s {
 	char * xmldata;
 };
 
+// This is used to store the pointers to the beginning of tags.
 struct xml_tag_obj_s {
 	xml_obj_type_t obj_type;
 	union xml_obj * right;
@@ -93,12 +112,15 @@ struct xml_tag_obj_s {
 	union xml_obj * children;
 };
 
+// This is used to store the special 'root' object; The start of the contents.
 struct xml_root_obj_s {
 	// This, being root, has no siblings
 	xml_obj_type_t obj_type;
 	union xml_obj * children;
 };
 
+
+/* Unions */
 
 // Define the union.
 typedef union xml_obj {
@@ -114,6 +136,7 @@ typedef union xml_obj {
 
 
 /* prototypes */
+
 void usage (char *name);
 int strip_comments(char *contents, signed long size);
 xml_obj_t * parse_xml_content(char * file_contents);
@@ -129,16 +152,20 @@ xml_obj_t * parse_xml_content(char * file_contents);
 int main(int argc, char *argv[])
 {
 	// Set variables.
-	char c;
-	char *read_file=NULL;
-	FILE *xml;
+	char c;			// The character being read from command line.
+	char *read_file=NULL;	// Pointer for the read file, set to NULL.
+	FILE *xml;		// The file pointer.
+	
 	// 'off_t' is defined in sys/types.h as the file size type. This
 	// usually equiv. to 'unsigned long' on *nix.
 	off_t buf_size;
-	// MADI: Read up structures.
+	
+	// This will hold the info on the file I am reading.
 	struct stat file_info;
+	
 	// Initial file read buffer
 	char *file_contents;
+	
 	// The root of the parsed stack
 	xml_obj_t * root;
 	
@@ -164,10 +191,12 @@ int main(int argc, char *argv[])
 			case '?':
 				if (optopt == 'f')
 				{
+					// -f was passed without a filename after it.
 					printf("Missing filename.\n");
 				}
 				else
 				{
+					// A switch was used that wasn't recognized.
 					printf("Invalid switch '%c'\n", optopt);
 				}
 				usage(argv[0]);
@@ -257,18 +286,20 @@ int main(int argc, char *argv[])
 		return -9;
 	}
 	
-	// Test.
+	// Test. This prints whatever is in 'file_contents'. Remove when done
 // 	printf("%s", file_contents);
 	
 	// Free and destroy the stack
 	destroy_stack();
 	
-	// Release the memory back to the OS that had been allocated to the file buffer's memory
+	// Release the memory back to the OS that had been allocated to the
+	// file buffer's memory
 	free(file_contents);
 	
 	// If I hit here, I am exiting normally.
 	return 0;
 }
+
 
 /* functions */
 
@@ -303,24 +334,23 @@ int strip_comments(char *contents, signed long size)
 	while((*src != 0) && (size > 0))
 	{
 		// If this is the less-than sign, evaluate.
-		// I put the most expensive checks on the right as the first check to fail ends all following checks.
+		// I put the most expensive checks on the right as the first
+		// check to fail ends all following checks.
 		if ((pi == 0) && (com == 0) && (cd == 0) && (*src == '<'))
 		{
 			if (*(src+1) == '?')
 			{
 				// In a processing instruction.
 				pi=1;
-// 				printf("\nEntering PI\n\n");
 				src++;
 				size--;
 			}
-			// 'strncmp', unlike 'strcmp', looks ahead only X chars for a match. Returns 0 on match.
+			// 'strncmp', unlike 'strcmp', looks ahead only X chars
+			// for a match. Returns 0 on match.
 			else if (strncmp(src+1, "!--", 3) == 0)
 			{
 				// In a comment
 				com=1;
-// 				printf("\nEntering COM\n\n");
-				// No need to check the next three characters.
 				src+=3;
 				size-=3;
 			}
@@ -328,15 +358,15 @@ int strip_comments(char *contents, signed long size)
 			{
 				// In a CDATA block.
 				cd = 1;
-// 				printf("\nEntering CD\n\n");
-				// I don't jump forward here because I am not stripping the '<![CDATA[' at this stage.
 			}
 		}
 		
-		// If I am not in a comment of processing instruction, record the character.
+		// If I am not in a comment of processing instruction, record
+		// the character.
 		if ((pi == 0) && (com == 0))
 		{
-			// Push the char from src to dst and move forward one position in dst's array.
+			// Push the char from src to dst and move forward one
+			// position in dst's array.
 			printf("%c", *src);
 			*dst++ = *src;
 		}
@@ -346,13 +376,15 @@ int strip_comments(char *contents, signed long size)
 		if ((cd == 1) && (*src == ']') && (strncmp(src+1, "]>", 2) == 0))
 		{
 			cd=0;
-			// I don't worry about looping twice more because I know what they are and they won't trigger com or pi.
+			// I don't worry about looping twice more because I
+			// know what they are and they won't trigger com or pi.
 		}
 		// Check for leaving comments.
 		if ((com == 1) && (*src == '-') && (strncmp(src+1, "->", 2) == 0))
 		{
 			com=0;
-			// Now jump because I don't want to record the closing brace.
+			// Now jump because I don't want to record the closing
+			// brace.
 			src += 2;
 			size -= 2;
 		}
@@ -376,7 +408,7 @@ int strip_comments(char *contents, signed long size)
 	return 0;
 }
 
-// Prints usage
+// Prints diximal usage.
 void usage (char *name)
 {
 	printf("Usage: %s -f /path/to/file.xml\n", name);
@@ -385,9 +417,12 @@ void usage (char *name)
 // This does the actual parsing. Returns a pointer to the root
 xml_obj_t * parse_xml_content(char * file_contents)
 {
-	// Root object, initialized to NULL to make sure I'm not looking at garbage.
+	// Root object, initialized to NULL to make sure I'm not looking at
+	// garbage.
 	xml_obj_t * root_object = NULL;
-	xml_obj_type_t state_type = xml_none; // This stores the type of object we're in.
+	
+	// This stores the type of object we're in.
+	xml_obj_type_t state_type = xml_none;
 	
 	// Current object
 	xml_obj_t * current_object;
@@ -402,6 +437,8 @@ xml_obj_t * parse_xml_content(char * file_contents)
 	// Set the type of the root object.
 	root_object->obj_type = xml_root;
 	// the above is equiv. to "*root_object.type"
+	
+	// Now, being the beginning, set 'current_object' to 'root_object'.
 	current_object = root_object;
 	
 	// Loop through until I hit 0.
@@ -412,17 +449,19 @@ xml_obj_t * parse_xml_content(char * file_contents)
 		if (*file_contents == '<')
 		{
 			// Yar. < is ALWAYS a delimeter, so \0 it.
-			*file_contents = 0;
-			file_contents++;
-			// This is equal; *file_contents++ = 0; because it sets and /then/ increments.
+			*file_contents++ = 0;
+			// This is equal to;
+			// *file_contents = 0; file_contents++;
+			// because it sets and /then/ increments.
 			
 			// Am I going in to a CDATA?
 			if (strncmp(file_contents, "![CDATA[", 8) == 0)
 			{
-				// Set the < to \0 so that I terminate the previous block.
-				file_contents += 8;	// Jump past '<![CDATA['
+				// Jump past '<![CDATA['
+				file_contents += 8;
 				
-				// I want my sibling to point to this new XML object.
+				// I want my sibling to point to this new XML
+				// object.
 				current_object->generic.right = calloc(1, sizeof(xml_obj_t));
 				if (current_object->generic.right == NULL)
 				{
@@ -430,36 +469,133 @@ xml_obj_t * parse_xml_content(char * file_contents)
 					return NULL;
 				}
 				
-				// Make the 'current_object' now point to the memory we just calloc'ed.
+				// Make the 'current_object' now point to the
+				// memory we just calloc'ed.
 				current_object = current_object->generic.right;
-				current_object->cdata.obj_type = xml_cdata;	// This lets me know later what I am looking at (xml_cdata)
-				current_object->cdata.xmldata = file_contents;	// This will mark the start of the CDATA.
 				
+				// This lets me know later what I am looking at
+				// (xml_cdata)
+				current_object->cdata.obj_type = xml_cdata;
+				
+				// This will mark the start of the CDATA.
+				current_object->cdata.xmldata = file_contents;
+				
+				// Loop through until I see the closing braces.
+				// I could do this with just the 'strncmp' but
+				// that is somewhat expensive. By checking for
+				// a ']', I won't run the 'strncmp' until I
+				// have a good reason to do so.
 				while ((*file_contents != ']') && (strncmp(file_contents+1, "]>", 2) != 0))
 				{
 					file_contents++;
 				}
 				
-				*file_contents = 0;	// This changes ']]>' to '\0]>'.
-				file_contents += 2;	// Jump past the ']>'
-				state_type = xml_none;	// Reset to xml_none as I don't know what I will be in the next loop around.
+				// This changes ']]>' to '\0]>'.
+				*file_contents = 0;
+				// Jump past the ']>'
+				file_contents += 2;
+				// Reset to xml_none as I don't know what I
+				// will be in the next loop around.
+				state_type = xml_none;
 			}
-			
-			
-			
-/*			else if ((isalpha(*(file_contents+1)) != 0) || (*(file_contents+1) == '_'))
+			else if ((isalpha(*(file_contents+1)) != 0) || (*(file_contents+1) == '_'))
 			{
 				// Tag name.
+				// This could be self-closing and it could contain 
 				state_type = xml_tag;
+				
+				// I want my sibling to point to this new XML
+				// object.
+				current_object->generic.right = calloc(1, sizeof(xml_obj_t));
+				if (current_object->generic.right == NULL)
+				{
+					// Failed to allocate memory.
+					return NULL;
+				}
+				
+				// Make the 'current_object' now point to the
+				// memory we just calloc'ed.
+				current_object = current_object->generic.right;
+				
+				// This lets me know later what I am looking at
+				// (xml_tag)
+				current_object->cdata.obj_type = xml_tag;
+				
+				// This will mark the start of the tag name.
+				current_object->xmltag.tagname = file_contents;
+				
+				// Loop through until I see either a space, a
+				// closing '>' or a self-terminating '/>'. When
+				// I see a space, I will loop until I see a
+				// non-space character and determine if it's an
+				// attribute or a (self)closing brace.
+				while ((*file_contents != ' ') && (*file_contents != '\t') && (*file_contents != '\n')
+					&& (*file_contents != '/') && (*file_contents != '>'))
+				{
+					file_contents++;
+				}
+				
+				// At this point, I've hit the end of the tag
+				// name one way or the other. So now I need to
+				// mark this as the end of the tag name and
+				// then sort out what I am looking at.
+				// To start, loop past all white spaces until I
+				// see something.
+				if ((*file_contents != ' ') && (*file_contents != '\t') && (*file_contents != '\n'))
+				{
+					// I am looking at a white space. Set
+					// it to '\0' and loop until I find
+					// something.
+					*file_contents = 0;
+					
+					// Set my state to none until I know
+					// what I am looking at again.
+					state_type = xml_none;
+					
+					// Now loop until I see something.
+					// MADI: Should this be a function? I
+					//       can see this being used again.
+					while ((*file_contents != ' ') && (*file_contents != '\t') && (*file_contents != '\n'))
+					{
+						file_contents++;
+					}
+				}
+				
+				// At this point, I know I am looking at a
+				// non-white-space character.
+				if ((*file_contents == '/') && (*file_contents+=1 == '>'))
+				{
+					// Self-closing tag.
+				}
+				else if (*file_contents == '>')
+				{
+					// Tag is closed. Now I'll need to push
+					// this tag on to the stack so that
+					// when I see a closing tag I can
+					// ensure that it matches.
+				}
+				else if (isalpha(*file_contents) != 0)
+				{
+					// I'm looking at the start of an
+					// attrib=value pair. This must allow
+					// for white-spaces around the '='
+					// sign.
+					// MADI: I should make this a function if only to make it more readable.
+				}
+				else
+				{
+					// Mal-formed XML.
+					return NULL;
+				}
+				
+				*file_contents++ = 0;
+				
+				
 			}
-			else if (*(file_contents+1) == '/')
+/*			else if (*file_contents+1 == '/')
 			{
-				// Closing a tag
-			}
-			else if (strncmp(file_contents+1, "![CDATA[", 8) == 0)
-			{
-				// CDATA block
-				state_type = xml_cdata;
+				// I'm looking at a closing tag. I'll need to
+				// make sure it matches the last opening tag.
 			}
 			else
 			{
